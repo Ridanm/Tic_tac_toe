6606adb87109e2634aa5aaaa30274bba866fd8fc
Ghost <danyfox1.dm@gmail.com>
2022-10-26 20:52:37 -0300

    check winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -20,146 +20,148 @@
   # Nombre del ganador si lo hay de lo contrario  --- @ganador
 
 
-module Info
-  WELCOME = '---  WELCOME TO TIC_TAC_TOE GAME  ---'
-end
-
-class Board 
-  attr_reader :board
-
-  def initialize
-    @board = []
-    create_board
+  module Info
+    WELCOME = '---  WELCOME TO TIC_TAC_TOE GAME  ---'
+    CHECK_WINNER = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   end
-
-  def create_board
-    (0..9).each do |let|
-      @board << let
+  
+  class Board 
+    attr_reader :board
+  
+    def initialize
+      @board = []
+      create_board
     end
-  end
-
-  def show_board
-    col_separator, row_separator = " | ", "--+---+--"
-    label_for_position = lambda{|position| @board[position] ? @board[position] : position}
-    row_for_display = lambda{|row| row.map(&label_for_position).join(col_separator)}
-   
-    row_positions = [[1,2,3], [4,5,6], [7,8,9]]
-    rows_for_display = row_positions.map(&row_for_display)
-    puts rows_for_display.join("\n" + row_separator + "\n")
-  end
-
-  def to_s 
-    "#{@board}"
-  end
-end
-
-
-class Player 
-  attr_reader :name
-  attr_accessor :letter
-
-  def initialize
-  end
-
-  def player_name
-    print 'Enter your name: '
-    @name = gets.chomp 
-  end
-
-  def choose_letter
-    until @letter == 'x' || @letter == 'o'
-      print 'Enter the letter with which you will play => x <=, => o <= '
-      @letter = gets.chomp 
-    end
-  end
-
-end
-
-
-class Game < Board 
-  attr_reader :player_one, :player_two, :board, :num 
-
-  def initialize(player_one, player_two)
-    @player_one = player_one
-    @player_two = player_two
-    @board = Board::new 
-    @num = @board.board.size - 1
-  end
-
-  def select_position!
-    p @num
-    @board.show_board
-
-    while @num > 0
-      change_player! @num
-      puts "#{@player.name.capitalize} select a free position in the board: "
-      @position = gets.chomp.to_i 
-      verifier_free_position @player
-      @board.show_board 
-    end
-  end
-
-  def change_player! player
-    if player.odd?
-      @player = @player_one
-    else
-      @player = @player_two
-    end
-  end
-
-  def verifier_free_position player
-      if @position.between?(1, 9) && @board.board[@position].class != ''.class 
-        @board.board[@position] = player.letter  
-        @num -= 1
-      else 
-        puts "Thath position is occupied, or the character does not match..."
+  
+    def create_board
+      (0..9).each do |let|
+        @board << let
       end
-  end
-
-  def winner 
-    
-  end
-
-end
-
-class Display
-  attr_reader :board
-
-  def initialize
-    @board = Board.new 
+    end
+  
+    def show_board
+      col_separator, row_separator = " | ", "--+---+--"
+      label_for_position = lambda{|position| @board[position] ? @board[position] : position}
+      row_for_display = lambda{|row| row.map(&label_for_position).join(col_separator)}
+     
+      row_positions = [[1,2,3], [4,5,6], [7,8,9]]
+      rows_for_display = row_positions.map(&row_for_display)
+      puts rows_for_display.join("\n" + row_separator + "\n")
+    end
+  
+    def to_s 
+      "#{@board}"
+    end
   end
   
-  def display_board
-    puts @board.show_board.class 
-  end
-end
-
-
-class Main 
   
-end
+  class Player 
+    attr_reader :name
+    attr_accessor :letter
+  
+    def initialize
+    end
+  
+    def player_name
+      print 'Enter your name: '
+      @name = gets.chomp 
+    end
+  
+    def choose_letter
+      until @letter == 'x' || @letter == 'o'
+        print 'Enter the letter with which you will play => x <=, => o <= '
+        @letter = gets.chomp 
+      end
+    end
+  
+  end
+  
+  
+  class Game < Board 
+    attr_reader :player_one, :player_two, :board, :num 
+    include Info 
+  
+    def initialize(player_one, player_two)
+      @player_one = player_one
+      @player_two = player_two
+      @board = Board::new
+      @num = @board.board.size - 1
+    end
+  
+    def select_position!
+      p @num
+      @board.show_board
+  
+      while @num > 0
+        change_player! @num
+        puts "#{@player.name.capitalize} select a free position in the board: "
+        @position = gets.chomp.to_i 
+        check_free_position @player
+        @board.show_board 
 
-# Iniciando participantes y letras 
-board = Board::new
-player_one = Player::new
-player_two = Player::new 
+        if winner @player 
+          puts "  ---  #{@player.name.capitalize} WIN!  ---"
+          return 
+        elsif board_full? @board 
+          puts "  ---  IS A DRAW!  ---"
+          return 
+        end
+      end
+    end
+  
+    def change_player! player
+      if player.odd?
+        @player = @player_one
+      else
+        @player = @player_two
+      end
+    end
+  
+    def check_free_position player
+        if @position.between?(1, 9) && @board.board[@position].class != ''.class 
+          @board.board[@position] = player.letter  
+          @num -= 1
+        else 
+          puts "Thath position is occupied, or the character does not match..."
+        end
+    end
+  
+    def winner player
+      CHECK_WINNER.any? do |line|
+        line.all? {|pos| @board.board[pos] == player.letter}
+      end
+    end
 
-puts Info::WELCOME
+    def board_full? board 
+      (1..9).all? {|pos| board.board[pos].is_a?(String) }
+    end
+  
+    def to_s
+      puts "#{@board}"
+    end
 
-puts "Player one !!!"
-player_one.player_name 
-player_one.choose_letter
-
-puts "Player two !!!"
-player_two.player_name 
-
-if player_one.letter == 'x' 
-  player_two.letter = 'o'
-else 
-  player_two.letter = 'x'
-end
-
-# Comienza el juego 
-game = Game::new(player_one, player_two)
-game.select_position!
-#game.change_player!
+  end
+  
+  # Starting game
+  board = Board::new
+  player_one = Player::new
+  player_two = Player::new 
+  
+  puts Info::WELCOME
+  
+  puts "Player one !!!"
+  player_one.player_name 
+  player_one.choose_letter
+  
+  puts "Player two !!!"
+  player_two.player_name 
+  
+  if player_one.letter == 'x' 
+    player_two.letter = 'o'
+  else 
+    player_two.letter = 'x'
+  end
+  
+  game = Game::new(player_one, player_two)
+  game.select_position!
+  
