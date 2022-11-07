2d90cfd9a0826e35f6de48492cb3d80ea857379d
Ghost <danyfox1.dm@gmail.com>
2022-11-07 20:28:56 -0300

    Replay game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tic_tac_toe
 Two players must achieve three in a row
+
+
--- main_toe.rb
+++ main_toe.rb
@@ -1,40 +1,38 @@
 require './tic_tac_toe'
 
-def play
-  board = Board::new
-  player_one = Player::new
-  player_two = Player::new 
+board = Board::new
+player_one = Player::new
+player_two = Player::new 
 
-  puts Info::WELCOME
+puts Info::WELCOME
 
-  puts "\nPlayer one !!!"
-  player_one.player_name 
-  player_one.choose_letter
+puts "Player one !!!"
+player_one.player_name 
+player_one.choose_letter
 
-  puts "\nPlayer two !!!"
-  player_two.player_name 
+puts "Player two !!!"
+player_two.player_name 
 
-  if player_one.letter == 'x' 
-    player_two.letter = 'o'
-  else 
-    player_two.letter = 'x'
-  end
-
-  game = Game::new(player_one, player_two)
-  game.select_position!
-end 
-
-def play_again
-  print "\nPLAY AGAIN PRESS => yes or no: "
-  yes = gets.chomp
-  if yes == 'yes'
-    play 
-  else
-    puts "\nThanks for playing"
-  end
-  puts 
+if player_one.letter == 'x' 
+  player_two.letter = 'o'
+else 
+  player_two.letter = 'x'
 end
 
-play 
-play_again
+game = Game::new(player_one, player_two)
+game.select_position!
+
+result = false 
+until result 
+  print "\nPLAY AGAIN PRESS => yes or no: "
+  result = gets.chomp
+  if result == 'yes'
+    game = Game::new(player_one, player_two)
+    game.select_position!
+    result = false 
+  else 
+    puts 'Thanks for playing' 
+    result = true 
+  end
+end
 
