dafd27fcd8e7dd0e0169c06159b3e0770af797c0
Ghost <danyfox1.dm@gmail.com>
2023-12-06 08:25:20 -0300

    Changing directories

---
+++ lib/main.rb
@@ -0,0 +1,39 @@
+require './tic_tac_toe.rb'
+require 'colorize'
+
+board = Board::new
+player_one = Player::new
+player_two = Player::new 
+
+puts Info::WELCOME
+
+puts "\nPlayer one !!!"
+player_one.player_name
+player_one.choose_letter
+
+puts "\nPlayer two !!!"
+player_two.player_name
+
+if player_one.letter == 'x' 
+  player_two.letter = 'o'
+else 
+  player_two.letter = 'x'
+end
+
+game = Game::new(player_one, player_two)
+game.select_position!
+
+result = false 
+until result 
+  print "\nPLAY AGAIN PRESS => yes or no: ".yellow
+  result = gets.chomp
+  if result == 'yes'
+    game = Game::new(player_one, player_two)
+    game.select_position!
+    result = false 
+  else 
+    puts 'Thanks for playing!!!'.green  
+    result = true 
+  end
+end
+
