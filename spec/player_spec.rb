c159de422584c1d3aff502f28c3bdfc413b8cfcc
Ghost <danyfox1.dm@gmail.com>
2023-12-09 09:23:48 -0300

    Player name test and choose letter methods are correct

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,18 @@
-require './tic_tac_toe.rb'
+require 'tic_tac_toe.rb'
 
-describe 'Player class' do 
+describe Player do 
 	before :each do 
 		@player = Player.new 
 	end
 
-	it 'enter name' do 
-		expect(@player.player_name).to eq 'Roberto'
+	it 'when enter name' do 
+		allow(@player).to receive(:gets).and_return('name')
+		expect(@player.player_name).to eq('name')
+	end
+
+	it 'select letter to play' do 
+		allow(@player).to receive(:gets).and_return('x')
+		expect(@player.choose_letter).to eq('x')
 	end
 end 
+
