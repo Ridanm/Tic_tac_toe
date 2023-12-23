72589778642d01b088ed7beafdd9c7cabfe851ea
Ghost <danyfox1.dm@gmail.com>
2023-12-23 07:02:11 -0300

    Adding identation

--- lib/board.rb
+++ lib/board.rb
@@ -9,8 +9,8 @@ class Board
   
   def show_board
     col_separator, row_separator = " | ".blue, "--+---+--".blue 
-    label_for_position = lambda{|position| @board[position] ? @board[position] : position}
-    row_for_display = lambda{|row| row.map(&label_for_position).join(col_separator)}
+    label_for_position = lambda{ |position| @board[position] ? @board[position] : position }
+    row_for_display = lambda{ |row| row.map(&label_for_position).join(col_separator) }
      
     row_positions = [[1,2,3], [4,5,6], [7,8,9]]
     rows_for_display = row_positions.map(&row_for_display)
