8daaf3f34adc03039b7f7373a258da2598a6af97
Ridanm <danyfox1.dm@gmail.com>
2025-05-25 11:57:45 -0300

    replay added

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,11 @@
-require 'colorize'
-require_relative 'dependencies.rb'
+# frozen_string_literal: true
 
-board = Board::new
+require 'colorize'
+require_relative 'dependencies'
+
+Board.new
 player_one = Player.new
-player_two = Player.new 
+player_two = Player.new
 
 puts Info::WELCOME
 
@@ -14,26 +16,25 @@ player_one.choose_letter
 puts "\nPlayer two !!!"
 player_two.player_name
 
-if player_one.letter == 'x' 
+if player_one.letter == 'x'
   player_two.letter = 'o'
-else 
+else
   player_two.letter = 'x'
 end
 
-game = Game::new(player_one, player_two)
+game = Game.new(player_one, player_two)
 game.select_position!
 
-result = false 
-until result 
+result = false
+until result
   print "\nPLAY AGAIN type => yes or any key to exit: ".yellow
   result = gets.chomp.downcase
   if result == 'yes'
-    game = Game::new(player_one, player_two)
+    game = Game.new(player_one, player_two)
     game.select_position!
-    result = false 
-  else 
-    puts 'Thanks for playing!!!'.green  
-    result = true 
+    result = false
+  else
+    puts 'Thanks for playing!!!'.green
+    result = true
   end
 end
-
