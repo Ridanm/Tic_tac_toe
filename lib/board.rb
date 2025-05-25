f12d016035183453de1481478956439632954834
Ridanm <danyfox1.dm@gmail.com>
2025-05-25 11:47:31 -0300

    #board changed by #boar_data

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 require 'colorize'
 
-class Board 
-  attr_reader :board
-  
+# This class makes the game board
+class Board
+  attr_reader :board_data
+
   def initialize
-    @board = (0..9).to_a
+    @board_data = (0..9).to_a
   end
-  
+
   def show_board
-    col_separator, row_separator = " | ".blue, "--+---+--".blue 
-    label_for_position = lambda{ |position| @board[position] ? @board[position] : position }
-    row_for_display = lambda{ |row| row.map(&label_for_position).join(col_separator) }
-     
-    row_positions = [[1,2,3], [4,5,6], [7,8,9]]
+    col_separator = ' | '.blue
+    row_separator = '--+---+--'.blue
+    label_for_position = ->(position) { @board_data[position] || position }
+    row_for_display = ->(row) { row.map(&label_for_position).join(col_separator) }
+
+    row_positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     rows_for_display = row_positions.map(&row_for_display)
-    puts "\n#{rows_for_display.join("\n" + row_separator + "\n")}\n\n"
+    puts "\n#{rows_for_display.join("\n#{row_separator}\n")}\n\n"
   end
 end
