1c98d3cef378547293c691754d423f7abef25a41
Ridanm <danyfox1.dm@gmail.com>
2025-05-25 11:54:06 -0300

    #player_name added capitalize, squeeze, strip

--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,21 @@
-class Player 
+# frozen_string_literal: true
+
+# This class implements the player and the choice of the letter to play
+class Player
   attr_reader :name
   attr_accessor :letter
-  
+
   def player_name
     print 'Enter your name: '
-    @name = gets.chomp 
-    player_name if @name == ''
-    @name 
+    @name = gets.chomp
+    player_name if @name == '' || @name == ' '
+    @name.capitalize.strip.squeeze
   end
-  
+
   def choose_letter
     puts "\nEnter the letter you want to play with it can be: X or: O"
     @letter = gets.chomp.downcase
     choose_letter unless @letter == 'x' || @letter == 'o'
-    @letter 
+    @letter
   end
 end
-  
