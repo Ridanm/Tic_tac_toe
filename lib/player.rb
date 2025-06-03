30853c89be881c2603c6091fdfbfb0d580328d92
Ridanm <danyfox1.dm@gmail.com>
2025-06-03 07:54:26 -0300

    When entering the name we apply capitalize strip squeeze

--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
+require_relative './dependencies'
+
 # This class implements the player and the choice of the letter to play
 class Player
   attr_reader :name
   attr_accessor :letter
 
+  include Info
+
   def player_name
     print 'Enter your name: '
-    @name = gets.chomp
+    @name = gets.chomp.capitalize.strip.squeeze
     player_name if @name == '' || @name == ' '
-    @name.capitalize.strip.squeeze
+    @name
   end
 
   def choose_letter
-    puts "\nEnter the letter you want to play with it can be: X or: O"
+    print Info::show('enter letter', name)
     @letter = gets.chomp.downcase
     choose_letter unless @letter == 'x' || @letter == 'o'
     @letter
