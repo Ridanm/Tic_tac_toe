8f71b7323b544edaa641969059ff84c101247847
Ridanm <ridanm@hotmail.com>
2026-03-08 07:46:50 -0300

    Tests reviewed by rubocop and corrected

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -53,14 +53,13 @@ RSpec.describe Game do
     end
   end
 
-    describe '#current_player!' do
-      it 'if the number is odd' do
+  describe '#current_player!' do
+    it 'if the number is odd' do
       expect(game.current_player!(1)).to eq(player_one)
-      end
+    end
 
-      it 'if the number is even' do
-      expect(game.current_player!(2)
-).not_to eq(player_one)
+    it 'if the number is even' do
+      expect(game.current_player!(2)).not_to eq(player_one)
     end
 
     it 'when number is 1' do
@@ -69,10 +68,13 @@ RSpec.describe Game do
   end
 
   describe 'Info module' do
+    let(:player_two_msj) { 'select a free position in the board: ' }
+
     context 'player name, select an empty box' do
       it 'when player_two' do
-        expect(Info).to receive(:show).with('free_position', player_two.name).and_return('Player_two', 'select a free position in the board: ').once.time
-        Info::show('free_position', player_two.name)
+        expect(Info).to receive(:show).with('free_position', player_two.name).and_return('Player_two',
+                                                                                         player_two_msj).once.time
+        Info.show('free_position', player_two.name)
       end
     end
   end
@@ -81,9 +83,9 @@ RSpec.describe Game do
     it 'when the box is occupied, or the number is not between 1 and 9' do
       expect(Info).to receive(:show).with('filled').and_return('Thath position is already filled, or the character
 does not match...')
-      Info::show('filled')
+      Info.show('filled')
     end
-   
+
     it 'When you enter a correct number' do
       allow(player_one).to receive(:gets).and_return(3)
       game.check_free_position(3)
@@ -95,7 +97,7 @@ does not match...')
     let(:check_winner) { [[1, 2, 3], [4, 5, 6]] }
     context '#winner' do
       before do
-        game.board.board_data[1] ='x'
+        game.board.board_data[1] = 'x'
         game.board.board_data[2] = 'x'
         game.board.board_data[3] = 'x'
       end
@@ -108,48 +110,50 @@ does not match...')
         expect(game.winner?(check_winner, player_two)).to be false
       end
     end
+  end
 
-    context '#board_full?' do
-      it 'when there are no more movements' do
-        (1..9).each { |i| game.board.board_data[i] = 'x' }
-        expect(game.board_full?(game.board.board_data)).to be true
-      end
+  describe '#board_full?' do
+    it 'when there are no more movements' do
+      (1..9).each { |i| game.board.board_data[i] = 'x' }
+      expect(game.board_full?(game.board.board_data)).to be true
+    end
 
-      it 'when the board is not full?' do
-        expect(game.board_full?(game.board.board_data)).to be false
-      end
+    it 'when the board is not full?' do
+      expect(game.board_full?(game.board.board_data)).to be false
+    end
 
-      it 'we display the message' do
-        expect(Info).to receive(:show).with('draw').and_return("\n  ---  Is a draw!!!  ---".green)
-        Info::show('draw')
+    it 'we display the message' do
+      expect(Info).to receive(:show).with('draw').and_return("\n  ---  Is a draw!!!  ---".green)
+      Info.show('draw')
+    end
+  end
+
+  describe '#game_over' do
+    let(:play_msj) { "\nPLAY AGAIN type => yes or any key to exit: ".yellow }
+
+    it 'while the game is running' do
+      expect(Info).to receive(:show).with('play_again').and_return(play_msj)
+      Info.show('play_again')
+    end
+
+    context '#continue_or_exit' do
+      it 'When the game ends and we repeat it' do
+        expect(game).to receive(:gets).and_return('yes')
+        game.continue_or_exit
       end
     end
- 
-    describe '#game_over' do
-      it 'while the game is running' do
-        expect(Info).to receive(:show).with('play_again').and_return("\nPLAY AGAIN type => yes or any key to exit: ".yellow)
-        Info::show('play_again')
-      end
 
-      context '#continue_or_exit' do
-        it 'When the game ends and we repeat it' do
-          expect(game).to receive(:gets).and_return('yes')
-          game.continue_or_exit
-        end
+    context '#repeat_game' do
+      it 'when you want to repeat the game' do
+        expect(game).to receive(:play)
+        game.play
       end
+    end
 
-      context '#repeat_game' do
-        it 'when you want to repeat the game' do
-          expect(game).to receive(:play)
-          game.play
-        end
-      end
-      
-      context 'exiting the game' do
-        it 'When the game ends and we leave' do
-          expect(game).to receive(:game_over).and_return('Thanks for playing!!!')
-          game.game_over
-        end
+    context 'exiting the game' do
+      it 'When the game ends and we leave' do
+        expect(game).to receive(:game_over).and_return('Thanks for playing!!!')
+        game.game_over
       end
     end
   end
